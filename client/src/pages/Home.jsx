7f99457998b2e0328ddbc9a2e206ff3ebc75f749
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-09-04 08:20:15 -0500

    feat: adds theme to local storage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,47 +1,23 @@
 /* DEPENDENCIES */
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { themes } from "../utils/themes";
 import Hourglass from "../components/Loading";
 import "../styles/home.css";
 
 const loading = false;
 
-const themes = [
-  {
-    id: 0,
-    name: "Default",
-    outer_text: "text-slate-900",
-    inner_text: "text-white",
-    dark: "bg-slate-900",
-    medium: "bg-blue-950",
-    light: "bg-slate-800",
-    clear_bg: "bg-slate-300",
-  },
-  {
-    id: 1,
-    name: "Neutral",
-    outer_text: "text-stone-900",
-    inner_text: "text-white",
-    dark: "bg-stone-900",
-    medium: "bg-stone-800",
-    light: "bg-neutral-900",
-    clear_bg: "bg-stone-300",
-  },
-  {
-    id: 2,
-    name: "Colorful",
-    outer_text: "text-black",
-    inner_text: "text-white",
-    dark: "bg-blue-950",
-    medium: "bg-cyan-700",
-    light: "bg-pink-900",
-    clear_bg: "bg-pink-100",
-  },
-];
-
 /* HOME PAGE */
 export default function Home() {
-  const [theme, setTheme] = useState(0);
+  // Get saved theme or set to default theme
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme !== null ? parseInt(savedTheme) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   if (loading) {
     return <Hourglass />;
---
+++ client/src/utils/themes.js
@@ -0,0 +1,32 @@
+export const themes = [
+  {
+    id: 0,
+    name: "Default",
+    outer_text: "text-slate-900",
+    inner_text: "text-white",
+    dark: "bg-slate-900",
+    medium: "bg-blue-950",
+    light: "bg-slate-800",
+    clear_bg: "bg-slate-300",
+  },
+  {
+    id: 1,
+    name: "Neutral",
+    outer_text: "text-stone-900",
+    inner_text: "text-white",
+    dark: "bg-stone-900",
+    medium: "bg-stone-800",
+    light: "bg-neutral-900",
+    clear_bg: "bg-stone-300",
+  },
+  {
+    id: 2,
+    name: "Colorful",
+    outer_text: "text-black",
+    inner_text: "text-white",
+    dark: "bg-blue-950",
+    medium: "bg-cyan-700",
+    light: "bg-pink-900",
+    clear_bg: "bg-pink-100",
+  },
+];
