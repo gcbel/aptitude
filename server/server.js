00b989c776575496e7f7eee0104f460ea2c710e2
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-09-03 19:38:40 -0500

    feat: tinkers with deployment

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ const startApolloServer = async () => {
   app.use(express.json());
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/index.html"));
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
 
