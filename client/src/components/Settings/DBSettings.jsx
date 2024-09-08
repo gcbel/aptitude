7f43602e487085f04f01f629f34fc28df5ef26d0
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-09-08 17:23:56 -0500

    feat: adds change username div

--- client/src/components/Settings/DBSettings.jsx
+++ client/src/components/Settings/DBSettings.jsx
@@ -3,27 +3,40 @@ import { useState } from "react";
 import { useTheme } from "../../utils/ThemeContext";
 
 /* DASHBOARD SETTINGS */
-export default function DBSettings(dashboard) {
+export default function DBSettings({ dashboard }) {
   const { theme, setTheme, themes } = useTheme();
 
-  const array = dashboard.dashboard;
   const [openDBSettings, setOpenDBSettings] = useState(false);
+  const [DBTheme, setDBTheme] = useState(dashboard.theme);
+
+  function changeTheme(index) {
+    setDBTheme(index);
+  }
 
   return (
     <div>
       <div className="subtitle playfair dashboard-title-div">
-        <h2 className="dashboard-title">{array.name} Dashboard</h2>
-        <p>{openDBSettings ? "∧" : "∨"}</p>
+        <h2 className="dashboard-title">{dashboard.name} Dashboard</h2>
+        <button
+          className="expand-profile"
+          onClick={() => setOpenDBSettings((prev) => !prev)}
+        >
+          {openDBSettings ? "∧" : "∨"}
+        </button>
       </div>
       <div className="dark-separator"></div>
       {openDBSettings && (
-        <div>
+        <div className="db-settings">
           {/* STYLE */}
-          <div className="style-settings">
-            {themes.map((theme) => (
+          <h3 className="subtitle playfair db-style-title">Style</h3>
+          <div className="db-style-settings">
+            {themes.map((theme, index) => (
               <div
                 key={theme.id}
-                className={`card ${theme.clear_bg} ${theme.outer_text} style-card`}
+                className={`card ${theme.clear_bg} ${
+                  theme.outer_text
+                } style-card ${DBTheme === index && "border border-black"}`}
+                onClick={() => changeTheme(index)}
               >
                 <div className={`mini-card ${theme.dark}`}></div>
                 <div className="right-style-cards">
@@ -33,8 +46,20 @@ export default function DBSettings(dashboard) {
               </div>
             ))}
           </div>
-          <h2 className="title playfair">Objectives</h2>
-          <div id="goal-settings"></div>
+
+          {/* CONTENT */}
+          <h3 className="subtitle playfair db-content-title">Content</h3>
+          <div className="montserrat db-content-settings">
+            <div className="db-title-setting">
+              <p>Dashboard name:</p>
+              <input
+                type="text"
+                id="db-name"
+                name="db-name"
+                placeholder={dashboard.name}
+              ></input>
+            </div>
+          </div>
         </div>
       )}
     </div>
