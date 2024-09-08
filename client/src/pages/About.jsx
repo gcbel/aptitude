01fc748cda40a230ccd24e47dc3aa97a2e0c65ee
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-09-08 11:13:51 -0500

    feat: title on about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,9 +7,11 @@ export default function Error() {
 
   return (
     <div className={`about-page montserrat ${themes[theme].outer_text}`}>
-      <h1 className="title">
-        Welcome to Your All-in-One Productivity Dashboard!
+      <h1 className="text-2xl lg:text-3xl text-center mx-6 sm:mx-12 md:mx-28 lg:mx-64 font-medium">
+        We set out to make a beautiful, goal-focused dashboard that allows users
+        to track everything in one place.
       </h1>
+      <p></p>
     </div>
   );
 }
